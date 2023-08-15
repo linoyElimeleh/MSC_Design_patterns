0ddd2780ca438dcc6ce4a568ca529acf76dbd86e
Linoy Elimeleh <lino1998y@gmail.com>
2023-08-15 18:59:22 +0300

    first commit

---
+++ GarageLogic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GarageLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
